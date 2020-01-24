2118bc07d99a2eb8d6525f256767bd3e8d292951
Mhaugh <59743671+Mhaugh@users.noreply.github.com>
2020-01-24 09:57:17 -0500

    Add initial files

---
+++ bank/src/main/java/com/revature/bankapp/BankingApp.java
@@ -0,0 +1,17 @@
+package com.revature.bankapp;
+
+public class BankingApp {
+	//UserObject currentUser;
+	String userDatabaseLoaction = "";
+	String accountDatabaseLocation = "";
+	//DatabasesObject databases  //live databases in memory
+	
+	
+/*	
+ * public static void main(String[] args) {
+ *		database == new database();
+ *		
+ *	}
+ *
+ */
+}
